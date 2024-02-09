339f330bfe88993f07584cc728410ff01a1c862c
Ada-23kumar <132913793+Ada-23kumar@users.noreply.github.com>
2024-02-09 23:55:43 +0530

    adding color on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,13 +4,15 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Alien invasion")
+    # set the background color.
+    bg_color = pygame.Color(23, 193, 199)
     # Game objects
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
-
+        # redraw the sereen during each pass through the loop.
+        screen.fill(bg_color)
         pygame.display.flip()
 
 run_game()
