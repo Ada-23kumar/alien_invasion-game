9fc833cd75bc01d2b34dda896e72c215259022d7
Ada-23kumar <132913793+Ada-23kumar@users.noreply.github.com>
2024-02-10 17:46:46 +0530

    adding ship image

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,14 +2,15 @@ import sys
 import pygame
 
 from settings import Settings
-
-
+from ship import Ship
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien invasion")
+    # make a ship
+    ship = Ship(screen)
     # set the background color.
     bg_color = pygame.Color(23, 193, 199)
     # Game objects
@@ -20,6 +21,7 @@ def run_game():
                 sys.exit()
         # redraw the sereen during each pass through the loop.
         screen.fill(bg_color)
+        ship.blitme()
         pygame.display.flip()
 
 run_game()
--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ class Settings():
     def __init__(self):
         """A class to store all setting for Alien Invasion."""
         # screen settings
-        self.screen_width = 800
-        self.screen_height = 600
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
